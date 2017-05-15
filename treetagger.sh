d95a783e7f53de6407b602600394e787179f6585
José Manuel Martínez Martínez <chozelinek@gmail.com>
2017-05-15 15:01:18 +0200

    fix path for input directory

--- treetagger.sh
+++ treetagger.sh
@@ -1,11 +1,10 @@
 ## Script for commands to tag corpus with TreeTagger
-echo "Compiling EuroParl corpus in '$DATA' ...."
 
 ## Path to the directory to save corpus data
 DATA=../data
-IDIR_EN=$DATA/xml_metadata/EN
-IDIR_ES=$DATA/xml_metadata/ES
-IDIR_DE=$DATA/xml_metadata/DE
+IDIR_EN=$DATA/xml_sentence/EN
+IDIR_ES=$DATA/xml_sentence/ES
+IDIR_DE=$DATA/xml_sentence/DE
 ODIR_EN=$DATA/treetagger/EN
 ODIR_ES=$DATA/treetagger/ES
 ODIR_DE=$DATA/treetagger/DE
@@ -16,11 +15,11 @@ mkdir -p $ODIR_ES
 mkdir -p $ODIR_DE
 
 ## English
-echo "Tagging English proceedings ...."
+echo "Tagging English proceedings with TreTagger ...."
 for i in $IDIR_EN/*.xml; do echo $i; tree-tagger-english < $i > $ODIR_EN/$(basename $i); done
 ## Spanish
-echo "Tagging Spanish proceedings ...."
+echo "Tagging Spanish proceedings with TreeTagger ...."
 for i in $IDIR_ES/*.xml; do echo $i; tree-tagger-spanish < $i > $ODIR_ES/$(basename $i); done
 ## German
-echo "Tagging German proceedings ...."
+echo "Tagging German proceedings with TreeTagger ...."
 for i in $IDIR_DE/*.xml; do echo $i; tree-tagger-german < $i > $ODIR_DE/$(basename $i); done
